671df272588069723c7fa7a8362c4d04a483e69b
Gaurav <gmpaliwal21@gmail.com>
2023-01-17 18:11:14 +0530

    Intial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/firebase_options.dart';
 
-import 'package:notes/view/login_view.dart';
-
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());

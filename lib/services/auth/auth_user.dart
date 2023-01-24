236a5432a3f59600ebf3b4da907663ea779f272b
Gaurav <gmpaliwal21@gmail.com>
2023-01-24 21:19:50 +0530

    10th-commit

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart' show User;
+import 'package:flutter/material.dart';
 
-@override
+@immutable
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
